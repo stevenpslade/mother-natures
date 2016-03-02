6569d48e713fd5a85c5d549406d411542c2cb973
stevenpslade <stevenpslade@gmail.com>
2016-03-01 20:19:55 -0800

    setup location page and finish mock style for menu

--- assets/js/mother-natures.js
+++ assets/js/mother-natures.js
@@ -1,6 +1,14 @@
 $(function() {
   
-  //
-
+  $("li").each(function(index) {
+    var navItem = $(this).text().toLowerCase();;
+    var re = new RegExp(navItem);
+    console.log(re);
+    if (re.test(window.location.href)) {
+      $(this).addClass("button");
+    } else if (/index/.test(window.location.href) && navItem === "menu") {
+      $(this).addClass("button");
+    }
+  });
 
 });
