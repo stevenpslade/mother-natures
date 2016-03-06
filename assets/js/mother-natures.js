942e08ae7e43e45234f1e95819c247804324d3b9
stevenpslade <stevenpslade@gmail.com>
2016-03-05 17:39:26 -0800

    add google maps api and location details

--- assets/js/mother-natures.js
+++ assets/js/mother-natures.js
@@ -1,9 +1,10 @@
 $(function() {
   
+  // switch green box depending on which page you are viewing
+  // possibly make button class present but hidden to avoid choppy entrance
   $("li").each(function(index) {
     var navItem = $(this).text().toLowerCase();;
     var re = new RegExp(navItem);
-    console.log(re);
     if (re.test(window.location.href)) {
       $(this).addClass("button");
     } else if (/index/.test(window.location.href) && navItem === "menu") {
@@ -12,3 +13,45 @@ $(function() {
   });
 
 });
+
+var map1;
+var map2;
+var map3;
+var myLatLng;
+function initMap() {
+  var mapRothesayAve = {lat: 45.299253, lng: -66.038516}
+  var mapMacallister = {lat: 45.307627, lng: -66.014442}
+  var mapBrunswick = {lat: 45.274205, lng: -66.060968}
+  map1 = new google.maps.Map(document.getElementById('map-rothesay-ave'), {
+    center: mapRothesayAve,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: mapRothesayAve,
+    map: map1,
+    title: 'Hello World!'
+  });
+
+  map2 = new google.maps.Map(document.getElementById('map-mcallister'), {
+    center: mapMacallister,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: mapMacallister,
+    map: map2,
+    title: 'Hello World!'
+  });
+
+  map3 = new google.maps.Map(document.getElementById('map-brunswick'), {
+    center: mapBrunswick,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: mapBrunswick,
+    map: map3,
+    title: 'Hello World!'
+  });
+}
