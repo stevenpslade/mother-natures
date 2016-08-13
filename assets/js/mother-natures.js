90f7e04568b26343b10f99f3b2db429c2a8b89ba
stevenpslade <stevenpslade@gmail.com>
2016-08-13 10:30:05 -0700

    adding next and previous arrow functionality to photo viewer

--- assets/js/mother-natures.js
+++ assets/js/mother-natures.js
@@ -1,3 +1,38 @@
+$(document).on('click', 'a.controls', function(){
+  var index = $(this).attr('href');
+  var src = $('ul#photo-grid li:nth-child('+ index +') img').attr('src');
+  $('.modal img').attr('src', src);
+
+  //increment and decrement that next and previous button index
+  var newPrevIndex = parseInt(index) - 1;
+  var newNextIndex = parseInt(newPrevIndex) + 2;
+   
+  if($(this).hasClass('previous')){
+      $(this).attr('href', newPrevIndex);
+      $('a.next').attr('href', newNextIndex);
+  }else{
+      $(this).attr('href', newNextIndex);
+      $('a.previous').attr('href', newPrevIndex);
+  }
+
+  //hide arrows if first or last image
+  var total = $('ul#photo-grid li').length + 1;
+  //hide next button
+  if(total === newNextIndex){
+      $('a.next').hide();
+  }else{
+      $('a.next').show()
+  }
+  //hide previous button
+  if(newPrevIndex === 0){
+      $('a.previous').hide();
+  }else{
+      $('a.previous').show()
+  }
+
+ return false;
+});
+
 $(function() {
   
   // switch green box depending on which page you are viewing
@@ -26,17 +61,24 @@ $(function() {
   //photo viewer modal
   $("#photo-grid li img").on("click", function() {
     var src = $(this).attr('src');
-    var img = '<img src="' + src + '"/>';
+    var img = '<div class="modal"><img src="' + src + '"/></div>';
+
+    var index = $(this).parent('li').index();
+    var rightArrow = '<a class="controls next" href="'+ (index+2) + '"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>';
+    var leftArrow = '<a class="controls previous" href="' + (index) + '"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>';
+
+    var html = leftArrow + img + rightArrow;
 
     //give img and show the model
-    $(".modal").html(img);
-    $(".modal-wrapper").show();
+    $(".modal-wrapper").html(html).show();
+    //this will hide or show the correct arrows:
+    $('a.controls').trigger('click');
   });
 
   //hide modal
-  $(".modal-wrapper").on("click", function() {
-    $(".modal-wrapper").hide();
-  });
+  // $(".modal-wrapper").on("click", function() {
+  //   $(".modal-wrapper").hide();
+  // });
 
 });
 
